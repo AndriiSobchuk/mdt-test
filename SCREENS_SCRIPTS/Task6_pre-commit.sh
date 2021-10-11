16cd539ec5229f418d9f1a3e7cc52c2749e7df9b
Andrii Sobchuk <anri.sobchuk@gmail.com>
2021-10-11 06:04:38 +0000

    Final commit of Week 1 project

--- SCREENS_SCRIPTS/Task6_pre-commit.sh
+++ SCREENS_SCRIPTS/Task6_pre-commit.sh
@@ -7,7 +7,7 @@
 
 #################################################### 
 
-if [[ $(grep -rnw --exclude-dir=hooks --exclude=README.md /home/andrii/material-design-template/ -e shit) ]]; then
+if [[ $(grep -rnw --exclude-dir={hooks,SCREENS_SCRIPTS} --exclude=README.md /home/andrii/material-design-template/ -e shit) ]]; then
 #FAIL SCENARIO
         echo "BLOCKED BY pre-commit policy! Check your code again!!!!!"
         echo $(date) the commit had been blocked by policy reason >> failed-commits-log.txt
