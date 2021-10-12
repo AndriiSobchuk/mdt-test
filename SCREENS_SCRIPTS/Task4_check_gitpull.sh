b9d3146ceab3e8c590037f6fbe99ed54a7f66ba6
AndriiSobchuk <86925275+AndriiSobchuk@users.noreply.github.com>
2021-10-12 09:38:05 +0300

    Update Task4_check_gitpull.sh

--- SCREENS_SCRIPTS/Task4_check_gitpull.sh
+++ SCREENS_SCRIPTS/Task4_check_gitpull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #!/usr/bin/env bash
 
-cd $HOME/material-design-template && /usr/bin/git pull origin
+cd $HOME/material-design-template && /usr/bin/git pull origin master
 echo $(date) " pull is successful" >> $HOME/material-design-template/pull_mdt.txt 
 
